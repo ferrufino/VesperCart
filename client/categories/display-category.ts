1f58d1d7f0573d3c2705b7dc4f6263b0eaed3af9
Luis Loera <loeraluis93@gmail.com>
2016-04-21 17:16:07 -0500

    Category loading bug corrected, search component added but still not working

--- client/categories/display-category.ts
+++ client/categories/display-category.ts
@@ -3,29 +3,39 @@ import 'zone.js/dist/zone';
 import {Component, OnInit} from 'angular2/core';
 import {RouteParams} from 'angular2/router';
 import {Categories} from '../../collections/categories';
-import {findProducts} from '../../server/find-products';
+import {ProductService} from '../product-detail/product-service';
+import {RouterLink} from 'angular2/router';
 import {Products} from '../../collections/products';
+import {bootstrap} from 'angular2-meteor-auto-bootstrap';
+import {Tracker} from 'meteor/tracker';
 
 @Component({
 	selector: 'display-category',
-	template: `
-	{{category.name}}
-	{{category._id}}
-	{{category.id}}
-	`
+	templateUrl: '/client/categories/display-category.html',
+	styleUrls: ['/client/styles/display-category.css'],
+	directives: [
+		RouterLink
+		],
+	providers: [ProductService]
 })
 
-export class DisplayCategory {
+export class DisplayCategory implements OnInit{
 	category: Object;
 	products: Array<Object>;
+	products2: Array<Object>;
 
-	/*perro: <Perro> //has el import del objeto
-
-	perro = methodclass(id); //has el import donde esta el metodo*/
-
-	constructor(params: RouteParams){
+	constructor(params: RouteParams, private _productService: ProductService){
 		var categoryId = params.get('categoryId');
 		this.category = Categories.findOne(categoryId);
 	}
+
+	ngOnInit(){
+		this.getProducts();
+	}
+
+	getProducts(){
+		this._productService.getProducts().then(products => this.products = products);
+	}
+
 
 }

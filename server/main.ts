3912012ac681408b1aac4df8d7a03852b700d79c
Luis Loera <loeraluis93@gmail.com>
2016-04-21 17:54:33 -0500

    products with json array updated

--- server/main.ts
+++ server/main.ts
@@ -1,1 +1,4 @@
-import {Products} from '../collections/products.ts';
+import {loadProducts} from './load-products.ts';
+import {Categories} from '../collections/categories.ts';
+
+Meteor.startup(loadProducts);

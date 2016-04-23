785b68a561a3583e0db6b26337bb469a5a2c17f4
Luis Loera <loeraluis93@gmail.com>
2016-04-23 02:48:57 -0500

    adds update and delete to admin component

--- client/admin/admin-component.ts
+++ client/admin/admin-component.ts
@@ -3,9 +3,7 @@ import 'zone.js/dist/zone';
 import {Component,NgZone} from 'angular2/core';
 import {Products} from '../../collections/products';
 import {Tracker} from 'meteor/tracker';
-import {bootstrap} from 'angular2-meteor-auto-bootstrap';
-
-
+import {FormBuilder, ControlGroup, Validators, Control} from 'angular2/common';
 
 @Component({
 	selector: 'admin-component',
@@ -15,5 +13,35 @@ import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 })
 
 export class AdminComponent {
+	products: Array<Object>;
+	selectedproduct: Object;
 
+	constructor (zone: NgZone) {
+    Tracker.autorun(() => zone.run(() => {
+		this.products = Products.find().fetch();
+		}));
+	}
+
+	onSelect(product){
+		this.selectedproduct=product;
+	}
+
+	 saveProduct(product) {
+    	Products.update(this.selectedproduct._id, {
+      $set: {
+        name: product.name,
+        description: product.description,
+        quantity: product.quantity,
+        category: product.category,
+        storeName: product.storeName
+      }
+    });
+    alert("product succesfully updated");
+  }
+
+  onRemove(selectedp){
+  	Products.remove(selectedp._id);
+  	alert("Product successfully removed");
+  	this.selectedproduct = null;
+  }
 }
--- client/search/search-component.ts
+++ client/search/search-component.ts
@@ -3,7 +3,6 @@ import 'zone.js/dist/zone';
 import {Component,NgZone} from 'angular2/core';
 import {Products} from '../../collections/products';
 import {Tracker} from 'meteor/tracker';
-import {bootstrap} from 'angular2-meteor-auto-bootstrap';
 import {RouterLink} from 'angular2/router';
 
 

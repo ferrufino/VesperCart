57ae10b38eb417f988bfa2b497ed8b60d65f312f
Luis Loera <loeraluis93@gmail.com>
2016-04-24 16:09:20 -0500

    still working on the login/signup, not finished

--- client/login/login-register-component.ts
+++ client/login/login-register-component.ts
@@ -2,7 +2,7 @@ import 'reflect-metadata';
 import 'zone.js/dist/zone';
 import {Component} from 'angular2/core';
 import {MATERIAL_DIRECTIVES} from "ng2-material/all";
-import {FORM_DIRECTIVES} from "angular2/common";
+import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators} from "angular2/common";
 
 @Component({
 	selector: 'login-register-component',
@@ -12,6 +12,23 @@ import {FORM_DIRECTIVES} from "angular2/common";
 })
 
 export class LoginRegisterComponent {
+  
+  signUpForm: ControlGroup;
+  constructor(){
+    let fb = new FormBuilder();
+
+    this.signUpForm = fb.group({
+        firstname: ['',Validators.required],
+        lastname: ['',Validators.required],
+        address: ['',Validators.required],
+        cardNum: ['',Validators.required],
+        securityCode: ['',Validators.required],
+        username: ['',Validators.required],
+        password:['',Validators.required],
+        phoneNum: ['',Validators.required]
+    });
+  }
+
   states = [
     'State','Ags', 'B.C', 'B.C.S', 'Camp', 'Chis', 'Chih', 'Coah', 'Col', 'CDMX', 'DGO', 'Gto', 'Gro', 'Hgo', 'Jal', 'EdoMex', 'Mich', 'Mor', 'Nay', 'N.L',
     'Oax', 'Pue', 'Qro', 'Q.Roo', 'S.L.P', 'Sin', 'Son', 'Tab', 'Tamps', 'Tlax', 'Ver', 'Yuc', 'Zac'
@@ -21,7 +38,7 @@ export class LoginRegisterComponent {
   loginAttempt(){
     alert('attempted Login');
   }
-  registerAttempt(){
+    registerAttempt(){
     alert('registration attempt');
   }
 }

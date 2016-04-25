d26fd25fc2cbab58d46212afaecadd59f9772d61
Gustavo Ferrufino <gustavo.fdlf@gmail.com>
2016-04-24 19:39:35 -0500

    adds import for carts in main ts

--- server/main.ts
+++ server/main.ts
@@ -1,4 +1,5 @@
 import {loadProducts} from './load-products.ts';
 import {Categories} from '../collections/categories.ts';
+import {Carts} from '../collections/carts.ts';
 import {Meteor} from 'meteor/meteor';
 Meteor.startup(loadProducts);
